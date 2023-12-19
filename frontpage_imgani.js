a271caa617f4fd645b187b773e793bf1b486f51f
1028ouo <shellyyi1028@gmail.com>
2023-12-19 23:21:06 +0800

    我把javascipt獨立出來

---
+++ frontpage_imgani.js
@@ -0,0 +1,19 @@
+window.addEventListener("load",function(){
+        let pot_img = document.querySelector(".display_pot");
+        pot_img.style.right = "-550px";
+
+        let tomato_img = document.querySelector(".display_tomato");
+        tomato_img.style.transform = "translate(130px,100px)";
+
+        let mushroom_img = document.querySelector(".display_mushroom");
+        mushroom_img.style.transform = "translate(50px,50px)";
+
+        let onion_img = document.querySelector(".display_onion");
+        onion_img.style.transform = "translate(-280px,30px)";
+
+        let green_onion_img = document.querySelector(".display_green_onion");
+        green_onion_img.style.transform = "translate(-400px,150px)";
+
+        let vegetable_img = document.querySelector(".display_vegetable");
+        vegetable_img.style.transform = "rotate(-20deg) translate(-380px,5px)";
+})
