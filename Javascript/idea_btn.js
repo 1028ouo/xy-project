0a55ac3a1d9616c4280fe0d0b3f2d8a3bb054100
yen1102 <h45599641@gmail.com>
2023-12-21 03:10:18 +0800

    新增隨機菜單按鈕js跟選單css

--- Javascript/idea_btn.js
+++ Javascript/idea_btn.js
@@ -1,7 +1,10 @@
 let noidea=document.getElementById("have_no_idea");
 function pickdishes(){
         let final="想不到就<br>";
-        final+=`牛肉麵`;  //這邊之後加之後我們有的食譜，先來碗牛肉麵嘻嘻
+        let dish_array = ["牛肉麵","蝦仁炒蛋","辣炒年糕","韓式炸雞","溏心蛋","大阪燒","玉米濃湯","青醬雞肉義大利麵"];
+        let dish = dish_array[Math.floor(0+Math.random()*dish_array.length)];
+        final+=dish;
+        console.log(dish);
         final+=`<button onclick="pickdishes()" style="margin-left:5px;">換一道</button>`;
         noidea.innerHTML=final;
 }
