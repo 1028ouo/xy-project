93cf023ddc53dc214659041f824bf2fa0cb98622
1028ouo <shellyyi1028@gmail.com>
2023-12-24 19:57:05 +0800

    微調按鈕跟隨機顯示菜單的字

--- Javascript/idea_btn.js
+++ Javascript/idea_btn.js
@@ -3,7 +3,7 @@ function pickdishes(){
         let final="想不到就<br>";
         let dish_array = ["牛肉麵","蝦仁炒蛋","辣炒年糕","韓式炸雞","溏心蛋","大阪燒","玉米濃湯","青醬雞肉義大利麵"];
         let dish = dish_array[Math.floor(0+Math.random()*dish_array.length)];
-        final+=dish;
+        final+=`<span class="random_dishes">${dish}</span>`;
         console.log(dish);
         final+=`<button onclick="pickdishes()" class="btn" style="margin-left:5px;">換一道</button>`;
         noidea.innerHTML=final;
